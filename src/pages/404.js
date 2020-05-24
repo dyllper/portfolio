52981a3d7887659eddc78177b1d242b4c3f99b36
Dylan Perkins <dyllper@gmail.com>
2020-05-24 17:30:30 -0500

    Initial checkin

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>The page that you are looking for cannot be found.</p>
   </Layout>
 )
 
