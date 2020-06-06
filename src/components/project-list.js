5ca5de399529e38df82b039042bdd920f151f7b4
Dylan Perkins <dyllper@gmail.com>
2020-06-05 21:38:58 -0500

    More work on styling projects list.

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+import styles from "../styles/project-list.module.scss"
+
 export default function ProjectList() {
   const data = useStaticQuery(graphql`
     query {
@@ -22,11 +24,18 @@ export default function ProjectList() {
     <>
       {data.allMarkdownRemark.edges.map((edge, index) => {
         const { title, path, blurb, url } = edge.node.frontmatter
+        const classList = `${styles.projectContainer} ${
+          index % 2 !== 0 ? "dark reverse" : ""
+        }`
+        //TODO: Figure out why the dark and reverse classes are not being applied properly.
+        //TODO: Get the background color to stretch across the screen and not be restricted.
+        // May have to get rid of margin: 0 auto on the <main></main> element of the layout and apply
+        // it to individual containers as needed.
         return (
-          <div key={index}>
-            <div className="projectImage">Image will go here</div>
-            <div className="projectInfo">
-              <h2>{title}</h2>
+          <div className={classList} key={index}>
+            <div className={styles.projectImage}>Image will go here</div>
+            <div className={styles.projectInfo}>
+              <h3>{title}</h3>
               <p>{blurb}</p>
               <a href={url}>Check it out here!</a>
             </div>
---
+++ src/projects/placeholder.md
@@ -0,0 +1,13 @@
+---
+title: "Placeholder"
+path: "/project/placeholder"
+url: "https://google.com"
+blurb: "Placeholder project for styling and layout purposes."
+technologies: "NextJS"
+githubClient: "https://github.com/dyllper"
+githubServer: ""
+---
+
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores mollitia similique aliquid dignissimos architecto laborum? Non impedit possimus omnis magni sed tempore, voluptatibus dolorem dolor aperiam, ipsum doloribus ipsa laudantium.
+
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores mollitia similique aliquid dignissimos architecto laborum? Non impedit possimus omnis magni sed tempore, voluptatibus dolorem dolor aperiam, ipsum doloribus ipsa laudantium.
