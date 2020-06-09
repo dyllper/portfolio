b5300e410a4d9b14d338b0c78dfbe72e3e25937e
Dylan Perkins <dyllper@gmail.com>
2020-06-08 21:37:58 -0500

    Initial work on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,10 +3,35 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import styles from "../styles/contact.module.scss"
+
 const LongPageNamePage = () => (
-  <Layout pageName="Long Page Name is Very Long">
+  <Layout pageName="Contact Me">
     <SEO title="Test" />
-    <p>Hello, this is a long page name!</p>
+    <div className={styles.contactContainer}>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere quis
+        repellat repudiandae autem assumenda voluptate, hic iusto sed temporibus
+        officiis veniam nesciunt. Voluptas quis adipisci fugit porro laudantium
+        tempore quas.
+      </p>
+      <form>
+        <ul className={styles.flexForm}>
+          <li>
+            <label htmlFor="email">EMAIL</label>
+            <input type="email" name="email" id="email" />
+          </li>
+          <li>
+            <label htmlFor="name">NAME</label>
+            <input type="text" name="name" id="name" />
+          </li>
+          <li>
+            <label htmlFor="message">MESSAGE</label>
+            <textarea name="message" id="message" rows="10"></textarea>
+          </li>
+        </ul>
+      </form>
+    </div>
   </Layout>
 )
 
