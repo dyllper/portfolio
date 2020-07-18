3bb5b47bfbab0149e02516c56285110f5d086497
Dylan Perkins <dyllper@gmail.com>
2020-07-18 00:37:38 -0500

    Updating default site info to not use the Gatsby Starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dylan Perkins`,
+    description: `Dylan Perkins is a Full Stack Software Developer based in Oklahoma.`,
+    author: `@dyllper`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
